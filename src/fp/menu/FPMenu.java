d1e0539be8569fd9f171ea26a2315d7685c1e79d
franc <franc@Pacopa-MSI>
2016-10-03 12:22:26 -0500

    Menu

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -13,19 +13,45 @@ public class FPMenu {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        menu();
+        calculaAreas();
     }
     
     public static int menu(){
         int opcion;
+        do{
         Scanner entrada = new Scanner(System.in);
         System.out.println("---- Calculo de areas ----");
-        System.out.println("Menu");
-        System.out.println("Inroduce la opcion deseada");
-        System.out.println("1.- Area de cuadrados");
-        System.out.println("2.- Area de circulos");
-        System.out.println("3.- Area de triangulos");
+        System.out.println("        Menu        ");
+        System.out.println("Inroduce la opcion deseada:");
+        System.out.println("1. Area de cuadrados");
+        System.out.println("2. Area de circulos");
+        System.out.println("3. Area de triangulos");
+        System.out.println("0. Salir");
         opcion = entrada.nextInt();
+        
+    }while(opcion<0 || opcion>=4);
         return opcion;
         
     }
+    public static float calculaAreas(int op){
+        float area;
+        switch(op){
+            case 1;
+                area = areaCuadrado();
+                break;
+            case 2;
+                area = areaCirculo();
+                break;
+            case 3;
+                area = areaTriangulo();
+                break;
+            default;
+                mensajeSalida();
+               
+        }
+        return area;
+    }
+    
 }
