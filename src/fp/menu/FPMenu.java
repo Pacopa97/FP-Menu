95ba10c3cb89b1196a718ef652304fa8a6befc04
franc <franc@Pacopa-MSI>
2016-10-03 22:50:56 -0500

    Menu

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -15,7 +15,7 @@ public class FPMenu {
         // TODO code application logic here
         
         menu();
-        calculaAreas();
+       
     }
     
     public static int menu(){
@@ -35,23 +35,56 @@ public class FPMenu {
         return opcion;
         
     }
-    public static float calculaAreas(int op){
-        float area;
+    public static float calculaArea(int op){
+        float area = 0;
         switch(op){
-            case 1;
+            case 1:
                 area = areaCuadrado();
                 break;
-            case 2;
+            case 2:
                 area = areaCirculo();
                 break;
-            case 3;
+            case 3:
                 area = areaTriangulo();
                 break;
-            default;
+            default:
                 mensajeSalida();
                
         }
         return area;
     }
+    public static float solicitarDatos(String tipo){
+        float dato;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca el: " + tipo);
+        dato = teclado.nextFloat();
+        return dato;
+    }
     
+    public static float areaCuadrado(){
+        float a,area;
+        a = solicitarDatos("lado");
+        area = a*a;
+        return area;
+    }
+     public static float areaCirculo(){
+        float r,area;
+        final double pi;
+        pi = 3.1416;
+        r = solicitarDatos("radio");
+        area = (float) (pi*Math.pow(r, 2));
+        return area;
+     }
+     
+     public static float areaTriangulo(){
+         float a,b,area;
+         a = solicitarDatos("altura");
+         b = solicitarDatos("base");
+         area = a*b/2;
+         return area;
+     }
+     
+     public static void mensajeSalida(){
+         System.out.println("Gracias por participar");
+     }
 }
